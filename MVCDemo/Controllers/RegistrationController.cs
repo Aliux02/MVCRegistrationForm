e4cd05b36e240a570efceb60c674bb70e1278809
Aliux <aliux02@gmail.com>
2021-06-30 10:45:07 +0300

    Add project files.

---
+++ MVCDemo/Controllers/RegistrationController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCDemo.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            var registrationModel = new Registration();
+
+            return View(registrationModel);
+        }
+
+        public IActionResult PostRegistrationData(Registration registrationModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("index", registrationModel);
+            }
+            return View(registrationModel);
+        }
+    }
+}
---
+++ MVCDemo/Models/Registration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCDemo.Models
+{
+    public class Registration
+    {
+        [DisplayName("Jusu Vardas")]
+        [Required (ErrorMessage ="Varda ivesti privaloma")]
+        [MinLength(2, ErrorMessage = "Iveskite ne maziau 3 simboliu")]
+        public string Name { get; set; }
+
+        [DisplayName("Jusu Pavarde")]
+        [Required(ErrorMessage = "Pavarde ivesti privaloma")]
+        [MinLength(3, ErrorMessage ="Iveskite ne maziau 3 simboliu")]
+        public string Surname { get; set; }
+
+        [DisplayName("Jusu Amžsius")]
+        [Range(0, 100, ErrorMessage = "Iveskite sveika skaiciu tarp 0 ir 100")]
+        [Required(ErrorMessage = "Amziu ivesti privaloma")]
+        public string Age { get; set; }
+
+        [DisplayName("Jusu Adresas")]
+        [Required (ErrorMessage = "Adresa ivesti privaloma")]
+        public  string Address { get; set; }
+
+    }
+}
